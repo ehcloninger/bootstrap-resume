bb28681f763e2cc7ff23ca344a011e9a1ff139cc
Eric Cloninger <ericc@pobox.com>
2017-07-21 13:33:15 -0500

    First commit with somewhat working contents

---
+++ content/posts/js/footer.js
@@ -0,0 +1,3 @@
+document.write(
+"<p class='footer-text'<small>This site available at <a href='https://github.com/ehcloninger/interactive-resume' target='_blank'>Github</a> under an <a href='http://www.apache.org/licenses/LICENSE-2.0' target='_blank'>Apache v2.0</a> license.<br/>Looking for <a href='../Eric_Cloninger_Resume.pdf' target='_blank'>traditional résumé</a>?</small></p>"
+);
---
+++ content/posts/js/header.js
@@ -0,0 +1,34 @@
+document.write(
+"<nav class='navbar navbar-default navbar-fixed-top'>" +
+"	<div class='container'>" +
+"		<div class='navbar-header'>" +
+"			<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>" +
+"               <span class='sr-only'> Toggle navigation </span> <span class='icon-bar'> </span> <span class='icon-bar'> </span> <span class='icon-bar'> </span>" +
+"           </button>" +
+"           <a class='navbar-brand' href='../../index.html'> Eric Cloninger </a> " +
+"		</div>" +
+"		<div id='navbar' class='collapse navbar-collapse'>" +
+"			<ul class='nav navbar-nav'>" +
+"				<li> <a href='../../index.html#cover'> Cover Letter </a> </li>" +
+"				<li> <a href='../../index.html#experience'> Experience </a> </li>" +
+"				<li> <a href='../../index.html#skills'> Skills </a> </li>" +
+"				<li> <a href='../../index.html#communications'> Communications </a> </li>" +
+"				<li class='dropdown'> <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'> Community <span class='caret'> </span> </a> " +
+"				<ul class='dropdown-menu'>" +
+"					<li> <a href='../../index.html#android'> Android </a> </li>" +
+"					<li> <a href='../../index.html#eclipse'> Eclipse </a> </li>" +
+"					<li> <a href='../../index.html#local'> Local </a> </li>" +
+"				</ul>" +
+"				</li>" +
+"			</ul>" +
+"			<ul class='nav navbar-nav navbar-right'>" +
+"				<li> <a href='http://www.linkedin.com/in/ericcloninger/' target='_blank'> <i class='fa fa-linkedin-square'> </i> </a> </li>" +
+"				<li> <a href='http://twitter.com/ericc' target='_blank'> <i class='fa fa-twitter-square'> </i> </a> </li>" +
+"				<li> <a href='https://github.com/ehcloninger' target='_blank'> <i class='fa fa-github-alt'> </i> </a> </li>" +
+"				<li> <a href='http://stackoverflow.com/users/296758/eric-cloninger' target='_blank'> <i class='fa fa-stack-overflow'> </i> </a> </li>" +
+"				<li> <a href='mailto:&#101;&#114;&#105;&#99;&#99;&#64;&#112;&#111;&#98;&#111;&#120;&#46;&#99;&#111;&#109;'> <i class='fa fa-envelope-o'> </i> </a> </li>" +
+"			</ul>" +
+"		</div>" +
+"	</div>" +
+"</nav>"
+);
