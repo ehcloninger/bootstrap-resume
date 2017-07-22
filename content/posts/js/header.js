9fabb8e1c345d9189d4b03f87ca1dc46bb0091cd
Eric Cloninger <ericc@pobox.com>
2017-07-22 13:32:56 -0500

    Add labels to header, fix some small issues

--- content/posts/js/header.js
+++ content/posts/js/header.js
@@ -16,11 +16,11 @@ document.write(
 "				<li> <a href='../../index.html#community'> Community </a> </li>" +
 "			</ul>" +
 "			<ul class='nav navbar-nav navbar-right'>" +
-"				<li> <a href='http://www.linkedin.com/in/ericcloninger/' target='_blank'> <i class='fa fa-linkedin-square fa-lg'> </i> </a> </li>" +
-"				<li> <a href='http://twitter.com/ericc' target='_blank'> <i class='fa fa-twitter-square fa-lg'> </i> </a> </li>" +
-"				<li> <a href='https://github.com/ehcloninger' target='_blank'> <i class='fa fa-github-alt fa-lg'> </i> </a> </li>" +
-"				<li> <a href='http://stackoverflow.com/users/296758/eric-cloninger' target='_blank'> <i class='fa fa-stack-overflow fa-lg'> </i> </a> </li>" +
-"				<li> <a href='mailto:&#101;&#114;&#105;&#99;&#99;&#64;&#112;&#111;&#98;&#111;&#120;&#46;&#99;&#111;&#109;'> <i class='fa fa-envelope-o fa-lg'> </i> </a> </li>" +
+"				<li> <a href='http://www.linkedin.com/in/ericcloninger/' target='_blank' title='LinkedIn'> <i class='fa fa-linkedin-square fa-lg'> </i> </a> </li>" +
+"				<li> <a href='http://twitter.com/ericc' target='_blank'> <i class='fa fa-twitter-square fa-lg' title='Twitter'> </i> </a> </li>" +
+"				<li> <a href='https://github.com/ehcloninger' target='_blank'> <i class='fa fa-github-alt fa-lg' title='Github'> </i> </a> </li>" +
+"				<li> <a href='http://stackoverflow.com/users/296758/eric-cloninger' target='_blank'> <i class='fa fa-stack-overflow fa-lg' title='StackOverflow'> </i> </a> </li>" +
+"				<li> <a href='mailto:&#101;&#114;&#105;&#99;&#99;&#64;&#112;&#111;&#98;&#111;&#120;&#46;&#99;&#111;&#109;'> <i class='fa fa-envelope-o fa-lg' title='E-Mail'> </i> </a> </li>" +
 "			</ul>" +
 "		</div>" +
 "	</div>" +
--- js/header.js
+++ js/header.js
@@ -4,7 +4,7 @@ function getBaseUrl() {
 }
 
 document.write(
-"<nav class='navbar navbar-default navbar-fixed-top'>/" +
+"<nav class='navbar navbar-default navbar-fixed-top'>" +
 "	<div class='container'>" +
 "		<div class='navbar-header'>" +
 "			<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>" +
@@ -21,11 +21,11 @@ document.write(
 "				<li> <a href='#community'> Community </a> </li>" +
 "			</ul>" +
 "			<ul class='nav navbar-nav navbar-right'>" +
-"				<li> <a href='http://www.linkedin.com/in/ericcloninger/' target='_blank'> <i class='fa fa-linkedin-square fa-lg'> </i> </a> </li>" +
-"				<li> <a href='http://twitter.com/ericc' target='_blank'> <i class='fa fa-twitter-square fa-lg'> </i> </a> </li>" +
-"				<li> <a href='https://github.com/ehcloninger' target='_blank'> <i class='fa fa-github-alt fa-lg'> </i> </a> </li>" +
-"				<li> <a href='http://stackoverflow.com/users/296758/eric-cloninger' target='_blank'> <i class='fa fa-stack-overflow fa-lg'> </i> </a> </li>" +
-"				<li> <a href='mailto:&#101;&#114;&#105;&#99;&#99;&#64;&#112;&#111;&#98;&#111;&#120;&#46;&#99;&#111;&#109;'> <i class='fa fa-envelope-o fa-lg'> </i> </a> </li>" +
+"				<li> <a href='http://www.linkedin.com/in/ericcloninger/' target='_blank' title='LinkedIn'> <i class='fa fa-linkedin-square fa-lg'> </i> </a> </li>" +
+"				<li> <a href='http://twitter.com/ericc' target='_blank'> <i class='fa fa-twitter-square fa-lg' title='Twitter'> </i> </a> </li>" +
+"				<li> <a href='https://github.com/ehcloninger' target='_blank'> <i class='fa fa-github-alt fa-lg' title='Github'> </i> </a> </li>" +
+"				<li> <a href='http://stackoverflow.com/users/296758/eric-cloninger' target='_blank'> <i class='fa fa-stack-overflow fa-lg' title='StackOverflow'> </i> </a> </li>" +
+"				<li> <a href='mailto:&#101;&#114;&#105;&#99;&#99;&#64;&#112;&#111;&#98;&#111;&#120;&#46;&#99;&#111;&#109;'> <i class='fa fa-envelope-o fa-lg' title='E-Mail'> </i> </a> </li>" +
 "			</ul>" +
 "		</div>" +
 "	</div>" +
