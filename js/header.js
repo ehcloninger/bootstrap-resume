f28722300a0f4fe08cd201a7922160e7ffb60c60
Eric Cloninger <ericc@pobox.com>
2018-03-11 14:42:33 -0500

    Update FontAwesome. Add link in header to main site.

--- js/header.js
+++ js/header.js
@@ -10,7 +10,8 @@ document.write(
 "			<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>" +
 "               <span class='sr-only'> Toggle navigation </span> <span class='icon-bar'> </span> <span class='icon-bar'> </span> <span class='icon-bar'> </span>" +
 "           </button>" +
-"           <a class='navbar-brand' href='index.html'> Eric Cloninger </a> " +
+"           <a class='navbar-brand' href='http://ericcloninger.com'><i class='fas fa-home'></i></a> " +
+"           <a class='navbar-brand' href='index.html'><i class='fas fa-arrow-circle-up'></i></a> " +
 "		</div>" +
 "		<div id='navbar' class='collapse navbar-collapse'>" +
 "			<ul class='nav navbar-nav'>" +
@@ -21,11 +22,11 @@ document.write(
 "				<li> <a href='#community'> Community </a> </li>" +
 "			</ul>" +
 "			<ul class='nav navbar-nav navbar-right'>" +
-"				<li> <a href='http://www.linkedin.com/in/ericcloninger/' target='_blank' title='LinkedIn'> <i class='fa fa-linkedin-square fa-lg'> </i> </a> </li>" +
-"				<li> <a href='http://twitter.com/ericc' target='_blank'> <i class='fa fa-twitter-square fa-lg' title='Twitter'> </i> </a> </li>" +
-"				<li> <a href='https://github.com/ehcloninger' target='_blank'> <i class='fa fa-github-alt fa-lg' title='Github'> </i> </a> </li>" +
-"				<li> <a href='http://stackoverflow.com/users/296758/eric-cloninger' target='_blank'> <i class='fa fa-stack-overflow fa-lg' title='StackOverflow'> </i> </a> </li>" +
-"				<li> <a href='mailto:&#101;&#114;&#105;&#99;&#99;&#64;&#112;&#111;&#98;&#111;&#120;&#46;&#99;&#111;&#109;'> <i class='fa fa-envelope-o fa-lg' title='E-Mail'> </i> </a> </li>" +
+"				<li> <a href='http://www.linkedin.com/in/ericcloninger/' target='_blank' title='LinkedIn'> <i class='fab fa-linkedin fa-lg'> </i> </a> </li>" +
+"				<li> <a href='http://twitter.com/ericc' target='_blank'> <i class='fab fa-twitter fa-lg' title='Twitter'> </i> </a> </li>" +
+"				<li> <a href='https://github.com/ehcloninger' target='_blank'> <i class='fab fa-github-alt fa-lg' title='Github'> </i> </a> </li>" +
+"				<li> <a href='http://stackoverflow.com/users/296758/eric-cloninger' target='_blank'> <i class='fab fa-stack-overflow fa-lg' title='StackOverflow'> </i> </a> </li>" +
+"				<li> <a href='mailto:&#101;&#114;&#105;&#99;&#99;&#64;&#112;&#111;&#98;&#111;&#120;&#46;&#99;&#111;&#109;'> <i class='far fa-envelope fa-lg' title='E-Mail'> </i> </a> </li>" +
 "			</ul>" +
 "		</div>" +
 "	</div>" +
